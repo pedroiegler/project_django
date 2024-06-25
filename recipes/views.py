f21b694550aca250947eda7f73062d4b1495d483
pedroiegler <pedroiegler1601@gmail.com>
2024-06-25 08:49:09 -0300

    feature: applying unit test

--- recipes/views.py
+++ recipes/views.py
@@ -1,25 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from utils.recipes.factory import make_recipe
-from recipes.models import Recipe, Category
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from recipes.models import Recipe
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('title')
 
-    return render(request, 'recipes/pages/index.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id, is_published=True)
+    recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id'))
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category | '
     })
 
 def recipe(request, id):
+    recipe = get_object_or_404(Recipe, id=id, is_published=True)
+
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
 
